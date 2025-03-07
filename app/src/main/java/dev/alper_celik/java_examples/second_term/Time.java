0c2eeabf3f56f9ce65674adc0dd6e529bdb36250
Alper Çelik <alper@alper-celik.dev>
2025-03-08 00:44:54 +0300

    add test for Time class and fix a bug in Time class

--- app/src/main/java/dev/alper_celik/java_examples/second_term/Time.java
+++ app/src/main/java/dev/alper_celik/java_examples/second_term/Time.java
@@ -2,79 +2,76 @@ package dev.alper_celik.java_examples.second_term;
 
 public class Time {
 
-  private int hour;
+    private int hour, minute, second;
 
-  public int getHour() {
-    return hour;
-  }
-
-  public void setHour(int hour) throws IllegalArgumentException {
-    if (hour < 24 || hour >= 0) {
-      this.hour = hour;
-    } else {
-      throw new IllegalArgumentException("hour can't be " + hour);
+    public int getHour() {
+        return hour;
     }
-  }
 
-  private int minute;
-
-  public int getMinute() {
-    return minute;
-  }
-
-  public void setMinute(int minute) throws IllegalArgumentException {
-    if (minute < 60 || minute >= 0) {
-      this.minute = minute;
-    } else {
-      throw new IllegalArgumentException("minute can't be " + minute);
+    public void setHour(int hour) throws IllegalArgumentException {
+        if (hour < 24 && hour >= 0) {
+            this.hour = hour;
+        } else {
+            throw new IllegalArgumentException("hour can't be " + hour);
+        }
     }
-  }
 
-  private int second;
-
-  public int getSecond() {
-    return second;
-  }
-
-  public void setSecond(int second) throws IllegalArgumentException {
-    if (second < 60 || second >= 0) { // lets ignore leap seconds
-      this.second = second;
-    } else {
-      throw new IllegalArgumentException("second can't be " + second);
+    public int getMinute() {
+        return minute;
     }
-  }
 
-  public Time(int hour, int minute, int second) {
-    setHour(hour);
-    setMinute(minute);
-    setSecond(second);
-  }
-
-  public void printTime() {
-    System.out.printf("%d:%d:%d\n", getHour(), getMinute(), getSecond());
-  }
-
-  public void incrementSecond() {
-    if (second < 59) {
-      second++;
-    } else if (minute < 59) {
-      minute++;
-      setSecond(0);
-    } else if (hour < 23) {
-      hour++;
-      setSecond(0);
-      setMinute(0);
-    } else {
-      setHour(0);
-      setMinute(0);
-      setSecond(0);
+    public void setMinute(int minute) throws IllegalArgumentException {
+        if (minute < 60 && minute >= 0) {
+            this.minute = minute;
+        } else {
+            throw new IllegalArgumentException("minute can't be " + minute);
+        }
     }
-  }
 
-  public void incrementBySeconds(int seconds) {
-    for (int i = 0; i < seconds; i++) {
-      incrementSecond();
+
+    public int getSecond() {
+        return second;
     }
-  }
+
+    public void setSecond(int second) throws IllegalArgumentException {
+        if (second < 60 && second >= 0) { // lets ignore leap seconds
+            this.second = second;
+        } else {
+            throw new IllegalArgumentException("second can't be " + second);
+        }
+    }
+
+    public Time(int hour, int minute, int second) {
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+    }
+
+    public String getTime() {
+        return String.format("%d:%d:%d", getHour(), getMinute(), getSecond());
+    }
+
+    public void incrementSecond() {
+        if (getSecond() < 59) {
+            setSecond(getSecond() + 1);
+        } else if (getMinute() < 59) {
+            setMinute(getMinute() + 1);
+            setSecond(0);
+        } else if (getHour() < 23) {
+            setHour(getHour() + 1);
+            setSecond(0);
+            setMinute(0);
+        } else {
+            setHour(0);
+            setMinute(0);
+            setSecond(0);
+        }
+    }
+
+    public void incrementBySeconds(int seconds) {
+        for (int i = 0; i < seconds; i++) {
+            incrementSecond();
+        }
+    }
 
 }
