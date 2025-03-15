f94b29e68ffd5d1c77ef41db23ef0d2143afadd1
Alper Çelik <alper@alper-celik.dev>
2025-03-15 21:56:45 +0300

    add merge_sort

--- app/src/main/java/dev/alper_celik/java_examples/second_term/Sorting.java
+++ app/src/main/java/dev/alper_celik/java_examples/second_term/Sorting.java
@@ -1,5 +1,7 @@
 package dev.alper_celik.java_examples.second_term;
 
+import java.util.Arrays;
+
 public class Sorting {
   public static void insertion_sort(Integer[] arr) {
     for (int i = 1; i < arr.length; i++) {
@@ -27,6 +29,65 @@ public class Sorting {
 
         }
       }
+    }
+  }
+
+  public static Integer[] merge_sort(Integer[] arr) {
+    return merge_sort_clas.merge_sort(arr);
+  }
+
+  private class merge_sort_clas {
+    public static Integer[] merge_sort(Integer[] arr) {
+
+      if (arr.length == 2) {
+        if (arr[0] > arr[1]) {
+          var tmp = arr[0];
+          arr[0] = arr[1];
+          arr[1] = tmp;
+        }
+        return arr;
+      }
+
+      if (arr.length < 2) {
+        return arr;
+      }
+
+      var half_arr_right = Arrays.copyOfRange(arr, 0, (arr.length / 2));
+      var half_arr_left = Arrays.copyOfRange(arr, (arr.length / 2), arr.length);
+
+      half_arr_right = merge_sort(half_arr_right);
+      half_arr_left = merge_sort(half_arr_left);
+
+      return merge(half_arr_right, half_arr_left);
+    }
+
+    public static Integer[] merge(Integer[] rhs_arr, Integer[] lhs_arr) {
+      var result = new Integer[rhs_arr.length + lhs_arr.length];
+      int right_key = 0, left_key = 0;
+
+      for (int i = 0; i < result.length; i++) {
+
+        if (right_key == rhs_arr.length) {
+          result[i] = lhs_arr[left_key];
+          left_key++;
+          continue;
+        }
+        if (left_key == lhs_arr.length) {
+          result[i] = rhs_arr[right_key];
+          right_key++;
+          continue;
+        }
+
+        if (rhs_arr[right_key] < lhs_arr[left_key]) {
+          result[i] = rhs_arr[right_key];
+          right_key++;
+        } else {
+          result[i] = lhs_arr[left_key];
+          left_key++;
+        }
+      }
+
+      return result;
     }
   }
 }
