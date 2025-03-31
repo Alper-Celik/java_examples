1ee1b795e8796e2cb5d6ea6dda0e97e1017ec833
Alper Çelik <alper@alper-celik.dev>
2025-04-01 01:28:50 +0300

    add Sorting unit tests

---
+++ app/src/test/java/dev/alper_celik/java_examples/second_term/SortingTests.java
@@ -0,0 +1,27 @@
+package dev.alper_celik.java_examples.second_term;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import dev.alper_celik.java_examples.utils.ArrayUtils;
+
+public class SortingTests {
+  static Stream<Function<Integer[], Integer[]>> methodProvider() {
+    return Stream.of(
+        ((x) -> Sorting.insertion_sort(x)),
+        ((x) -> Sorting.bouble_sort(x)),
+        ((x) -> Sorting.merge_sort(x)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("methodProvider")
+  void sortingTests(Function<Integer[], Integer[]> fun) {
+    var random_array = Sorting.generate_random_array(100_000, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    assertTrue(ArrayUtils.isSortedFromMinToMax(fun.apply(random_array)));
+  }
+}
