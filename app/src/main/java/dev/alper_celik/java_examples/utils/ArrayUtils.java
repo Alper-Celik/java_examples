a34e46462c7fe55343378bd2b0245dfcdf119d66
Alper Çelik <alper@alper-celik.dev>
2025-04-01 01:03:45 +0300

    add isSortedFromMinToMax method to ArrayUtils for future sorting alghrotims tests

--- app/src/main/java/dev/alper_celik/java_examples/utils/ArrayUtils.java
+++ app/src/main/java/dev/alper_celik/java_examples/utils/ArrayUtils.java
@@ -3,6 +3,15 @@ package dev.alper_celik.java_examples.utils;
 import java.util.List;
 
 public class ArrayUtils {
+
+  public static boolean isSortedFromMinToMax(Integer[] arr) {
+    for (int i = 0; i <= arr.length - 2; i++) {
+      if (arr[i] > arr[i + 1])
+        return false;
+    }
+    return true;
+  }
+
   public static <T> String arrayToString(T[] array) {
     var sb = new StringBuilder();
     sb.append("[ ");
