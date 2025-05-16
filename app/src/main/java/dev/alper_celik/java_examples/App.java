d613fcfe9ea1a88b530459a015a800e79481ca70
Alper Çelik <alper@alper-celik.dev>
2025-05-16 09:09:51 +0300

    add gui examples

--- app/src/main/java/dev/alper_celik/java_examples/App.java
+++ app/src/main/java/dev/alper_celik/java_examples/App.java
@@ -5,15 +5,19 @@ import java.util.concurrent.*;
 import dev.alper_celik.java_examples.second_term.*;
 import dev.alper_celik.java_examples.utils.ArrayUtils;
 import dev.alper_celik.java_examples.utils.MethodUtils;
+import dev.alper_celik.java_examples.second_term.forms.user_login;
 
 public class App {
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-    Integer[] arr = ArrayUtils.generate_random_array(100_000_000, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    Integer[] result = null;
-    var dur = MethodUtils.execution_time_of(() -> {
-      return Sorting.merge_sort(arr);
-    });
-    System.out.println(dur.getKey());
+    // Integer[] arr = ArrayUtils.generate_random_array(100_000_000,
+    // Integer.MIN_VALUE, Integer.MAX_VALUE);
+    // Integer[] result = null;
+    // var dur = MethodUtils.execution_time_of(() -> {
+    // return Sorting.merge_sort(arr);
+    // });
+    // System.out.println(dur.getKey());
+    //
+    user_login.main(args);
   }
 }
